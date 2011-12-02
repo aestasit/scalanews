b6e3f20bbbb7d7be74ca0e1b02758d32d422e4e8
Luciano Fiandesio <luciano@aestasit.com>
2011-12-02 11:48:13 +0100

    added news to sql

--- db/evolutions/default/2.sql
+++ db/evolutions/default/2.sql
@@ -1,6 +1,14 @@
 # --- !Ups
 
 insert into profile (id, username, password, email) values (1001, 'luciano', 'ecb4599131b1a179017931d0f58d43a05b02d570', 'aaa@hotmail.com');
+insert into profile (id, username, password, email) values (1002, 'aestas', 'ecb4599131b1a179017931d0f58d43a05b02d570', 'aestasit@hotmail.com');
+
+insert into story (id, title, story, profileId, created, votes, rank) values (2001, 'The Scala Programming language', 'http://www.scala-lang.org', 1001,'2011-12-02 14:55:21', 1, 0);
+insert into story (id, title, story, profileId, created, votes, rank) values (2002, 'Twitter Scala School', 'http://twitter.github.com/scala_school/', 1002,'2011-12-02 15:55:21', 1, 0);
+
 
 # --- !Downs
+delete from story;
 delete from profile;
+
+
